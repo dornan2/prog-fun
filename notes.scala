63bcfbd976f6ac93be5836695883f123ffaaa960
dornan2 <adrian.dornan2@mail.dcu.ie>
2018-04-06 17:52:06 +0100

    1

---
+++ notes.scala
@@ -0,0 +1,14 @@
+PROG Fun 
+------------------------------
+
+Call-by-value: has the advantage that it evaluates every function argument only once
+	-reduce the values first
+
+Call-by-name: has the advantage that a function argument is not evaluated if the parameter is unused
+	-remove the name first
+
+
+Expressions vs Statements
+
+Expression is a combination of variables, operations and values that yields a result value.
+Statements represent an action or command e.g print statements, assignment statements.
